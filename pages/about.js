a7e7a4f51088c41e057effbc7399db755de4ada9
Andrii Kicha <akicha@star.global>
2023-06-08 11:06:36 -0600

    add about page

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import Content from "components/atoms/Content";
 import Section from "components/atoms/Section";
 import Text from "components/atoms/Text";
 import Flex from "components/atoms/Flex";
+import PortfolioPresentationCard from "components/organisms/PortfolioPresentationCard";
 import Page from "components/templates/Page";
 import Image from "next/image";
 import Box from "components/atoms/Box";
@@ -27,7 +28,7 @@ export default function About() {
   return (
     <Page
       title="About Me"
-      description="Anastasiia Naumets is an independent designer & technologist. Previously at Carefull, TTF, & Google."
+      description="Anastasiia Naumets is a Product Designer using research & data to craft unique, measurable experiences that drive business impact."
     >
       <Landing>
         <Content>
@@ -36,121 +37,84 @@ export default function About() {
               preset="xLargeHeading"
               css={{ maxWidth: "30ch", width: "100%" }}
             >
-              Anastasiia Naumets is a designer & technologist. Previously at{" "}
-              <Link href="https://getcarefull.com">Carefull</Link>,{" "}
-              <Link href="https://truetoform.design">TTF</Link>, &{" "}
-              <Link href="https://google.com">Google</Link>.
-              <br />
-              Now independent.
-              <br />
+              Anastasiia Naumets is a Product Designer using research & data to
+              craft unique, measurable experiences that drive business impact.
             </Text>
           </Box>
-          <Box>
-            <Text preset="subHeading">A bit about me</Text>
-            <Grid columns={2} gapSize="large">
-              <Text css={{ color: "$gray300" }}>
-                I started designing at 13. Living in a rural area with a limited
-                internet connection, I lugged our family’s iMac to a friend’s
-                house to download a (totally legal) copy of adobe illustrator.{" "}
-                <br />
-                <br />
-                I’ve approached my career since then with a similar amount of
-                vigour. Always seeking to learn, understand, and evolve. I find
-                joy in the process of piecing together small parts of the built
-                world. Guided by the hope that my work can help to create
-                products that are subservient to the individual, systems that
-                empower human agency, and experiences that foster understanding.
-              </Text>
-              <Text css={{ color: "$gray300" }}>
-                When I’m not designing, you can find me obsessively reading,
-                spreadsheeting, or annoying my wife about my obsession of the
-                month. Past episodes have included: optimization of
-                tax-advantage investment accounts, the perfect t-shirt,
-                correcting my broken body due to 10+ years of sitting at a
-                computer, & much more. Feel free to ask and I’ll fill you into
-                the featured selection. <br />
-                <br />
-                I’m currently based in Grand Rapids, Michigan. The city I went
-                to college, met my wife, and a place that seems to consistently
-                pull me back.
-              </Text>
-            </Grid>
+          <Flex align="center" justify="center">
+            <Image
+              src="/images/about_1.jpg"
+              alt="Picture of a mother and son embracing eachother. A purple ring surrounds them showing protection."
+              width={555}
+              height={311}
+            />
+          </Flex>
+          <Box css={{ margin: "0 auto", width: 555 }}>
+            <Text css={{ margin: "48px 0 16px" }} preset="heading">
+              Hi there, nice to meet you!
+            </Text>
+            <Text>
+              I’m Anastasiia and usually go by “Nastia”✌️. I’m a Product
+              Designer currently working at QuoteToMe.
+              <br />
+              <br />
+              In my previous roles, I designed a major optimization of degree
+              management tool at HighPoint; delivered exceptional experiences
+              for global brands such as Goodwill, Breville, Omron, Chefsteps,
+              Hunkemöller at Astound Commerce; led the design of an automation
+              solution that improved processes and staff performance at Nokas.
+              <br />
+              <br />
+              I’m happiest when I exchange my thoughts and experiences with
+              others and I do that by educating entry-level designers at
+              Projector Institute, where they learn UX basics and complete their
+              first real-world projects.
+              <br />
+              <br />
+              I’m based in Edmonton, AB, but originally from tiny town of Rivne,
+              Ukraine. I have a big love for traveling, hiking, reading Sci-Fi,
+              and taking long walks with my dogs.
+            </Text>
           </Box>
           <Section>
-            <Text
-              preset="overline"
-              css={{ color: "$gray300", paddingBottom: "$space400" }}
-            >
-              A bunch of photos of me, you’re welcome
-            </Text>
             <Flex gap align="start">
               <Flex direction="column" gap>
-                <AssetCard caption="This what I look like roughly. Less serious most of the time.">
-                  <Image
-                    src="/images/aaronporter_about_1@2x.jpg"
-                    alt="Picture of a mother and son embracing eachother. A purple ring surrounds them showing protection."
-                    width={1104}
-                    height={1544}
-                  />
-                </AssetCard>
-                <AssetCard caption="Always looking to capture an authentic moment.">
-                  <Image
-                    src="/images/aaronporter_about_3@2x.jpg"
-                    alt="Picture of a mother and son embracing eachother. A purple ring surrounds them showing protection."
-                    width={1104}
-                    height={1508}
-                  />
-                </AssetCard>
-                <AssetCard caption="I go outside sometimes!">
-                  <Image
-                    src="/images/aaronporter_about_5@2x.jpg"
-                    alt="Picture of a mother and son embracing eachother. A purple ring surrounds them showing protection."
-                    width={1104}
-                    height={1508}
-                  />
-                </AssetCard>
+                <Box>
+                  <Image src="/images/about_2.jpg" width={552} height={736} />
+                </Box>
+                <Box>
+                  <Image src="/images/about_4.jpg" width={552} height={736} />
+                </Box>
+                <Box>
+                  <Image src="/images/about_6.jpg" width={552} height={736} />
+                </Box>
               </Flex>
               <Flex direction="column" gap>
-                <AssetCard caption="Fitting in a bit of work in Acadia National Park. I'm really great at work life balance!">
-                  <Image
-                    src="/images/aaronporter_about_2@2x.jpg"
-                    alt="Picture of a mother and son embracing eachother. A purple ring surrounds them showing protection."
-                    width={1104}
-                    height={1396}
-                  />
-                </AssetCard>
-                <AssetCard caption="Married up.">
-                  <Image
-                    src="/images/aaronporter_about_4@2x.jpg"
-                    alt="Picture of a mother and son embracing eachother. A purple ring surrounds them showing protection."
-                    width={1104}
-                    height={1526}
-                  />
-                </AssetCard>
-
-                <AssetCard caption="Always wear your protect gear.">
-                  <Image
-                    src="/images/aaronporter_about_6@2x.jpg"
-                    alt="Picture of a mother and son embracing eachother. A purple ring surrounds them showing protection."
-                    width={1104}
-                    height={1352}
-                  />
-                </AssetCard>
+                <Box>
+                  <Image src="/images/about_3.jpg" width={552} height={736} />
+                </Box>
+                <Box>
+                  <Image src="/images/about_5.jpg" width={552} height={736} />
+                </Box>
+                <Box>
+                  <Image src="/images/about_7.jpg" width={552} height={736} />
+                </Box>
               </Flex>
             </Flex>
           </Section>
-          <Section>
-            <Box css={{ margin: "0 auto", textAlign: "center" }}>
-              <Text preset="largeHeading">
-                That's all. Hire me or something.
-              </Text>
-              <Box css={{ marginTop: "$space400" }}>
-                <CopyToClipboardButton value="hello@aaronporter.co">
-                  hello@aaronporter.co
-                </CopyToClipboardButton>
-              </Box>
-            </Box>
-          </Section>
+          <PortfolioPresentationCard />
+          {/*<Section>*/}
+          {/*  <Box css={{ margin: "0 auto", textAlign: "center" }}>*/}
+          {/*    <Text preset="largeHeading">*/}
+          {/*      That's all. Hire me or something.*/}
+          {/*    </Text>*/}
+          {/*    <Box css={{ marginTop: "$space400" }}>*/}
+          {/*      <CopyToClipboardButton value="hello@aaronporter.co">*/}
+          {/*        hello@aaronporter.co*/}
+          {/*      </CopyToClipboardButton>*/}
+          {/*    </Box>*/}
+          {/*  </Box>*/}
+          {/*</Section>*/}
         </Content>
       </Landing>
     </Page>
